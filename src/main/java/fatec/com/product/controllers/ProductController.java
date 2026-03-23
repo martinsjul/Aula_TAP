21afdbc660d1535a42448ed06bbc1316bf92c3c3
martinsjul <jmr94424@gmail.com>
2026-03-23 14:15:56 -0300

    Testando

--- src/main/java/fatec/com/product/controllers/ProductController.java
+++ src/main/java/fatec/com/product/controllers/ProductController.java
@@ -3,20 +3,22 @@ package fatec.com.product.controllers;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fatec.com.product.models.Product;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-        @GetMapping
-        public ArrayList<Product> getProducts() {
+    private ArrayList<Product> products = new ArrayList();
 
-            //Declara um variavel do tipo Product
-            //Cria um objeto do tipo Product e atribui a variavel
+    public ProductController(){
+          
             Product p1 = new Product();
             p1.setId(1L);
             p1.setName("Notebook");
@@ -28,17 +30,34 @@ public class ProductController {
             p2.setName("Smartphone");
             p2.setPrice(2000.00);
             p2.setDescription("Smartphone android 128gb");
+
             Product p3 = new Product();
             p3.setId(3L);
             p3.setName("Tablet");
             p3.setPrice(1500.00);
             p3.setDescription("Tablet android 64gb");
 
-            ArrayList<Product> products = new ArrayList<>();
             products.add(p1);
             products.add(p2);   
             products.add(p3);
 
+             
+
+    }      
+
+        @GetMapping("/{}id")
+        public Product geProductById(@PathVariable long id) {
+            return products
+                .stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElse(null);
+           
+        }
+
+
+        @GetMapping
+        public ArrayList<Product> getProducts() {
             return products;
         }
 
